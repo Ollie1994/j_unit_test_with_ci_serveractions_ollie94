d1d037285d25eb2212769ca36db5f24e9b582245
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-12-20 17:44:02 +0100

    fail test

--- src/main/java/com/example/ProductsAPI/services/ProductService.java
+++ src/main/java/com/example/ProductsAPI/services/ProductService.java
@@ -1,6 +1,5 @@
 package com.example.ProductsAPI.services;
 
-import com.example.ProductsAPI.dto.ProductDTO;
 import com.example.ProductsAPI.models.Product;
 import com.example.ProductsAPI.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +22,7 @@ public class ProductService {
      * @throws IllegalArgumentException if required fields are missing or invalid.
      */
     // ignorera varningen, den kommer pga ingen controller
-    public Product createProduct(ProductDTO productDTO) {
+    public Product createProduct(Product product) {
 
         if (productDTO.getName() == null || productDTO.getName().trim().isEmpty()) {
             throw new IllegalArgumentException("Product name cannot be null or empty.");
