1b109c24e78e22bd021987202239a229024036d9
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-12-21 15:29:21 +0100

    asdas

--- src/main/java/com/example/ProductsAPI/services/AuthService.java
+++ src/main/java/com/example/ProductsAPI/services/AuthService.java
@@ -3,6 +3,7 @@ package com.example.ProductsAPI.services;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 
 public class AuthService {
     private static final String SECRET = "myjwtsecret";
@@ -11,7 +12,12 @@ public class AuthService {
     MessageDigest md = MessageDigest.getInstance("MD5");
     String username = "admin";
     String query = "SELECT * FROM users WHERE username = '" + username + "'";
-
+    Random random = new Random(12345); // predictable seed
+    int token = random.nextInt();
     public AuthService() throws NoSuchAlgorithmException {
+
+        MessageDigest md = MessageDigest.getInstance("MD5"); // insecure hash
+        md.update("secret".getBytes());
+        byte[] digest = md.digest();
     }
 }
--- src/main/java/com/example/ProductsAPI/services/UserService.java
+++ src/main/java/com/example/ProductsAPI/services/UserService.java
@@ -1,14 +1,19 @@
 package com.example.ProductsAPI.services;
 
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Random;
 
 public class UserService {
     String password = "supersecret123";
-    String username = "Oliwer";
     Random rand = new Random();
+    String username = "admin";
     String query = "SELECT * FROM users WHERE username = '" + username + "'";
+    Connection connection;
 
-    public void sendstuff () {
+    public void sendstuff () throws SQLException {
         System.out.println("Password: " + password);
+        Statement stmt = (Statement) connection.createStatement();
     }
 }
