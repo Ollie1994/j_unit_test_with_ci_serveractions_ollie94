073970c50acaa1b5542b65f435caaf862a2d22ca
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-12-21 14:42:36 +0100

    Test

--- src/main/java/com/example/ProductsAPI/services/UserService.java
+++ src/main/java/com/example/ProductsAPI/services/UserService.java
@@ -1,6 +1,14 @@
 package com.example.ProductsAPI.services;
 
-public class UserService {
-    private String password = "supersecret123"; // 🔥 likely triggers "hardcoded secret"
+import java.util.Random;
 
+public class UserService {
+    String password = "supersecret123";
+    String username = "Oliwer";
+    Random rand = new Random();
+    String query = "SELECT * FROM users WHERE username = '" + username + "'";
+
+    public void sendstuff () {
+        System.out.println("Password: " + password);
+    }
 }
